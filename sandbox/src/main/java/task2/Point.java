9368ce9e20f3e3dddb125cf7aceeb39a6ad26c5c
anaximines <anaximines@GM.Dlink>
2016-07-18 03:22:22 +0300

    Создан класс Point с методом distance
    Метод определяет расстояние между двумя точками на координатной
    плоскости

---
+++ sandbox/src/main/java/task2/Point.java
@@ -0,0 +1,14 @@
+package task2;
+
+/**
+ * Created by anaximines on 18/07/16.
+ */
+
+public class Point {
+  public double x; //the point on x-axis
+  public double y; //the point on y-axis
+
+  public double distance(Point point2){
+    return Math.sqrt(Math.pow((x-point2.x),2) + Math.pow((y - point2.y),2));
+  }
+}
