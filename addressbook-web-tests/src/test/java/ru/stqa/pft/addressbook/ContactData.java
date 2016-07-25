7354d299f5c1330f4befcd43d2b25f7c27ecc8e0
anaximines <miowch@gmail.com>
2016-07-25 21:16:48 +0300

    Создан набор тестов на создание контакта
    По аналогии создания набора на создание группы:
    1. Выделены вспомогательные методы.
    2. Созданы параметризованные методы.
    3. Параметры объединены в атрибуты вспомогательного класса.

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,115 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+    private final String firstName;
+    private final String middleName;
+    private final String lastName;
+    private final String nickname;
+    private final String title;
+    private final String company;
+    private final String address;
+    private final String homeTel;
+    private final String mobileTel;
+    private final String workTel;
+    private final String faxTel;
+    private final String email;
+    private final String email2;
+    private final String email3;
+    private final String homepage;
+    private final String address2;
+    private final String homeTel2;
+    private final String notes;
+
+    public ContactData(String firstName, String middleName, String lastName, String nickname, String title, String company, String address, String homeTel, String mobileTel, String workTel, String faxTel, String email, String email2, String email3, String homepage, String address2, String homeTel2, String notes) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.nickname = nickname;
+        this.title = title;
+        this.company = company;
+        this.address = address;
+        this.homeTel = homeTel;
+        this.mobileTel = mobileTel;
+        this.workTel = workTel;
+        this.faxTel = faxTel;
+        this.email = email;
+        this.email2 = email2;
+        this.email3 = email3;
+        this.homepage = homepage;
+        this.address2 = address2;
+        this.homeTel2 = homeTel2;
+        this.notes = notes;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getHomeTel() {
+        return homeTel;
+    }
+
+    public String getMobileTel() {
+        return mobileTel;
+    }
+
+    public String getWorkTel() {
+        return workTel;
+    }
+
+    public String getFaxTel() {
+        return faxTel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getEmail2() {
+        return email2;
+    }
+
+    public String getEmail3() {
+        return email3;
+    }
+
+    public String getHomepage() {
+        return homepage;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public String getHomeTel2() {
+        return homeTel2;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+}
