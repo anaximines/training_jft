3dc41a0c8597eb4abf4730a5320907d11f9c3e97
anaximines <anaximines@GM.Dlink>
2016-07-25 03:07:26 +0300

    Созданы два конструктора в классе Point
    1. Создание точки по умолчанию в начале координат.
    2. Создание точки с заданными координатами.

--- sandbox/src/main/java/Point.java
+++ sandbox/src/main/java/Point.java
@@ -6,6 +6,16 @@ public class Point {
   public double x; //the point on x-axis
   public double y; //the point on y-axis
 
+  public Point () {
+    this.x = 0.0;
+    this.y = 0.0;
+  }
+
+  public Point (double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
   public double distance(Point point2){
     return Math.sqrt(Math.pow((x-point2.x),2) + Math.pow((y - point2.y),2));
   }
