f2854be5f07663ee7aedc63e212c20691fcabae8
anaximines <anaximines@GM.Dlink>
2016-07-26 05:31:51 +0300

    Создан набор тестов на создание контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -12,4 +12,16 @@ public class ContactCreationTests extends TestBase {
         app.getContactHelper().submitContactCreation();
         app.getNavigationHelper().returnToHomePage();
     }
+
+    @Test
+    public void testNextContactCreation() {
+        app.getContactHelper().initContactCreation();
+        app.getContactHelper().fillContactInfo(new ContactData("firstName", "middleName", "lastName", "nickname", "title", "company", "address", "homeTel", "mobileTel", "workTel", "faxTel", "email", "email2", "email3", "homepage", "secondaryAddress", "secondaryHome", "secondaryNotes"));
+        app.getContactHelper().submitContactCreation();
+        app.getContactHelper().initNextContactCreation();
+        app.getContactHelper().fillContactInfo(new ContactData("firstName", "middleName", "lastName", "nickname", "title", "company", "address", "homeTel", "mobileTel", "workTel", "faxTel", "email", "email2", "email3", "homepage", "secondaryAddress", "secondaryHome", "secondaryNotes"));
+        app.getContactHelper().submitContactCreation();
+        app.getNavigationHelper().returnToHomePage();
+
+    }
 }
