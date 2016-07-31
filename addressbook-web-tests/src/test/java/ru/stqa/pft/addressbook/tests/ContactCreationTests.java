f2e77f9f90487d683959d02895d63e01c93cdec3
anaximines <anaximines@GM.Dlink>
2016-07-31 19:41:48 +0300

    Изменены значения передаваемых параметров при заполнении форм
    Некоторые значения заменены на null, чтобы либо не заполнять атрибут,
    либо не перезаписывать его.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -8,7 +8,7 @@ public class ContactCreationTests extends TestBase {
     @Test
     public void testContactCreation() {
         app.getContactHelper().initContactCreation();
-        app.getContactHelper().fillContactInfo(new ContactData("firstName", "middleName", "lastName", "nickname", "title", "company", "address", "homeTel", "mobileTel", "workTel", "faxTel", "email", "email2", "email3", "homepage", "secondaryAddress", "secondaryHome", "secondaryNotes"));
+        app.getContactHelper().fillContactInfo(new ContactData("firstName", null, "lastName", null, null, null, "address", null, "mobileTel", null, null, "email", null, null, null, null, null, null));
         app.getContactHelper().submitContactCreation();
         app.getNavigationHelper().returnToHomePage();
     }
@@ -16,10 +16,10 @@ public class ContactCreationTests extends TestBase {
     @Test
     public void testNextContactCreation() {
         app.getContactHelper().initContactCreation();
-        app.getContactHelper().fillContactInfo(new ContactData("firstName", "middleName", "lastName", "nickname", "title", "company", "address", "homeTel", "mobileTel", "workTel", "faxTel", "email", "email2", "email3", "homepage", "secondaryAddress", "secondaryHome", "secondaryNotes"));
+        app.getContactHelper().fillContactInfo(new ContactData("firstName", null, "lastName", null, null, null, null, "homeTel", "mobileTel", null, null, "email", null, "email3", null, null, "secondaryHome", null));
         app.getContactHelper().submitContactCreation();
         app.getContactHelper().initNextContactCreation();
-        app.getContactHelper().fillContactInfo(new ContactData("firstName", "middleName", "lastName", "nickname", "title", "company", "address", "homeTel", "mobileTel", "workTel", "faxTel", "email", "email2", "email3", "homepage", "secondaryAddress", "secondaryHome", "secondaryNotes"));
+        app.getContactHelper().fillContactInfo(new ContactData("firstName", null, "lastName", null, null, null, "address", "homeTel", "mobileTel", "workTel", null, "email", "email2", "email3", null, null, "secondaryHome", null));
         app.getContactHelper().submitContactCreation();
         app.getNavigationHelper().returnToHomePage();
 
