8d6f9373e7f9f52cd08e69c9c76caef25d1b2cca
anaximines <miowch@gmail.com>
2016-09-01 18:30:12 +0300

    Реализовано сохранение данных в формате json и параметризованы тесты на создание групп и контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,27 +1,59 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.thoughtworks.xstream.XStream;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
+    @DataProvider
+    public Iterator<Object[]> validContactsFromXml() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+        String xml = "";
+        String line = reader.readLine();
+        while (line != null) {
+            xml += line;
+            line = reader.readLine();
+        }
+        XStream xstream = new XStream();
+        xstream.processAnnotations(ContactData.class);
+        List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+        return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+    }
 
-    @Test
-    public void testContactCreation() {
+    @DataProvider
+    public Iterator<Object[]> validContactsFromJson() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
+        String json = "";
+        String line = reader.readLine();
+        while (line != null) {
+            json += line;
+            line = reader.readLine();
+        }
+        Gson gson = new Gson();
+        List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType());
+        return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+    }
+
+    @Test (dataProvider = "validContactsFromXml")
+    public void testContactCreation(ContactData contact) {
         File photo = new File("src/test/resources/geopic.png");
         Contacts before = app.contact().all();
-        ContactData contact = new ContactData().
-                withFirstName("firstName").
-                withLastName("lastName").
-                withAddress("address").
-                withMobilePhone("mobileTel").
-                withPhoto(photo);
-
+        contact.withPhoto(photo);
         app.contact().create(contact);
         app.contact().gotoToHomePage();
 
@@ -31,20 +63,15 @@ public class ContactCreationTests extends TestBase {
         assertThat(after, equalTo(before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
     }
 
-    @Test
-    public void testNextContactCreation() {
+    @Test (dataProvider = "validContactsFromJson")
+    public void testNextContactCreation(ContactData contact1) {
         Contacts before = app.contact().all();
-        ContactData contact1 = new ContactData().
-                withFirstName("firstNameF").
-                withLastName("lastNameF").
-                withMobilePhone("mobileTelF").
-                withGroup("test1");
         ContactData contact2 = new ContactData().
                 withFirstName("firstNameS").
                 withLastName("lastNameS").
                 withAddress("addressS").
                 withMobilePhone("mobileTelS").
-                withGroup("[none]");
+                withGroup("test1");
 
         app.contact().create(contact1);
         app.contact().initNextCreation();
@@ -60,5 +87,4 @@ public class ContactCreationTests extends TestBase {
 
         assertThat(after, equalTo(before.withAdded(contact1).withAdded(contact2)));
     }
-
 }
