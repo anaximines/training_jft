95043d85d279b1fd833f29ed8a8d0f530abf6183
anaximines <anaximines@GM.Dlink>
2016-09-14 01:35:57 +0300

    Добавлен новый модуль для тестов для Mantis

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/TestBase.java
@@ -0,0 +1,26 @@
+package ru.stqa.pft.mantis;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.stqa.pft.mantis.appmanager.ApplicationManager;
+
+/**
+ * Created by akuzina on 25.07.2016.
+ */
+public class TestBase {
+
+  protected static final ApplicationManager app =
+          new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
+
+  @BeforeSuite
+  public void setUp() throws Exception {
+    app.init();
+  }
+
+  @AfterSuite (alwaysRun = true)
+  public void tearDown() {
+    app.stop();
+  }
+
+}
